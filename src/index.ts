5d0e2ba3cfaf94637580b0c0c66eb3c03a44805c
Karel Heyse <karel.heyse@oqton.com>
2024-04-08 10:11:43 +0200

    omit the undefined type when the object property is marked as optional

--- src/index.ts
+++ src/index.ts
@@ -134,12 +134,17 @@ const zodToTsNode = (
 			const properties = Object.entries(zod._def.shape())
 
 			const members: ts.TypeElement[] = properties.map(([key, value]) => {
-				const nextZodNode = value as ZodTypeAny
-				const type = zodToTsNode(nextZodNode, ...otherArguments)
+				let nextZodNode = value as ZodTypeAny
 
 				const { typeName: nextZodNodeTypeName } = nextZodNode._def
 				const isOptional = nextZodNodeTypeName === 'ZodOptional' || nextZodNode.isOptional()
 				const hasDefault = nextZodNodeTypeName === 'ZodDefault'
+
+				if (nextZodNodeTypeName === 'ZodOptional') {
+					nextZodNode = nextZodNode.unwrap()
+				}
+
+				const type = zodToTsNode(nextZodNode, ...otherArguments)
 
 				const propertySignature = f.createPropertySignature(
 					undefined,
